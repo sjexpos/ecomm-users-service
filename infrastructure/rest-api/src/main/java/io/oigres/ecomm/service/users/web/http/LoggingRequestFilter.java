ba0c0609518e6c0283edb395041c9a4bc3dd0281
Sergio Exposito <sjexpos@gmail.com>
2024-10-01 20:56:43 -0300

    Add parameters=true on compilation and exclude paths in Logging filter

--- infrastructure/rest-api/src/main/java/io/oigres/ecomm/service/users/web/http/LoggingRequestFilter.java
+++ infrastructure/rest-api/src/main/java/io/oigres/ecomm/service/users/web/http/LoggingRequestFilter.java
@@ -3,6 +3,8 @@ package io.oigres.ecomm.service.users.web.http;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -18,7 +20,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LoggingRequestFilter implements Filter {
-	private MemoryMXBean memoryBean;
+	static private final List<Pattern> EXCLUDED_PATHS = List.of(
+			Pattern.compile("/"),
+			Pattern.compile("/favicon.ico"),
+			Pattern.compile("/actuator"),
+			Pattern.compile("/actuator/.*"),
+			Pattern.compile("/swagger"),
+			Pattern.compile("/swagger/.*"),
+			Pattern.compile("/swagger-ui"),
+			Pattern.compile("/swagger-ui/.*"),
+			Pattern.compile("/api/swagger-config")
+	);
+
+	private final MemoryMXBean memoryBean;
 
 	public LoggingRequestFilter() {
 		this.memoryBean = ManagementFactory.getMemoryMXBean();
@@ -28,20 +42,22 @@ public class LoggingRequestFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		String requestUri = httpRequest.getRequestURI();
-		if (requestUri.contains("swagger")) {
+		String method = httpRequest.getMethod() != null ? httpRequest.getMethod() : "";
+		String requestUri = httpRequest.getRequestURI() != null ? httpRequest.getRequestURI() : "";
+		String queryString = httpRequest.getQueryString() != null ? httpRequest.getQueryString() : "";
+		if (EXCLUDED_PATHS.stream().anyMatch(p -> p.matcher(requestUri).matches())) {
 			chain.doFilter(request, response);
 			return;
 		}
 		long start = System.currentTimeMillis();
 		long startHeap = memoryBean.getHeapMemoryUsage().getUsed();
-		log.info(String.format("BEGIN REQUEST %s %s %s", httpRequest.getMethod(), httpRequest.getRequestURI(), httpRequest.getQueryString()));
+		log.info(String.format("BEGIN - %s %s %s", method, requestUri, queryString));
         try {
             chain.doFilter(request, response);
         } finally {
             long end = System.currentTimeMillis();
 			long endHeap = memoryBean.getHeapMemoryUsage().getUsed();
-            log.info(String.format("END REQUEST %s %s - status: %s - time: %dms - heap: %s", httpRequest.getMethod(), httpRequest.getRequestURI(), httpResponse.getStatus(), (end-start), FileUtils.byteCountToDisplaySize(endHeap-startHeap) ));
+            log.info(String.format("END - %s %s - status: %s - time: %dms - heap: %s", method, requestUri, httpResponse.getStatus(), (end-start), FileUtils.byteCountToDisplaySize(endHeap-startHeap) ));
         }
 	}
 
